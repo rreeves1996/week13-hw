b83e4478d43d6781754ffa41111975fcafa7c76e
Ryan Reeves <reeves0ryan@gmail.com>
2022-08-07 03:03:16 -0700

    Everything works, need README

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,12 +39,40 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!tagData) {
+      res.status(404);
+    };
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json('Invalid tag ID!');
+    }
+
+    res.status(200).json("Tag successfully deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
